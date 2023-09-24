8ac7606bf791848baf99fa18d08a4cc033346bc2
WereF0X <unconfigured@null.spigotmc.org>
2023-09-24 18:31:41 +0200

    Added No Crystal, Bows and Crossbows

--- src/main/java/net/mcreator/gemsfabricmod/procedures/NocrystalProcedure.java
+++ src/main/java/net/mcreator/gemsfabricmod/procedures/NocrystalProcedure.java
@@ -7,6 +7,7 @@ import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.commands.CommandSourceStack;
@@ -37,6 +38,32 @@ public class NocrystalProcedure {
 					_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level() instanceof ServerLevel ? (ServerLevel) _ent.level() : null, 4,
 							_ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent), "ban @p End Crystals are banned on this server");
 				}
+			}
+		}
+		if (itemstack.getItem() == Items.BOW) {
+			{
+				Entity _ent = entity;
+				if (!_ent.level().isClientSide() && _ent.getServer() != null) {
+					_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level() instanceof ServerLevel ? (ServerLevel) _ent.level() : null, 4,
+							_ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent), "kick @p Bows are banned on this server");
+				}
+			}
+			if (entity instanceof Player _player) {
+				ItemStack _stktoremove = new ItemStack(Items.BOW);
+				_player.getInventory().clearOrCountMatchingItems(p -> _stktoremove.getItem() == p.getItem(), 1, _player.inventoryMenu.getCraftSlots());
+			}
+		}
+		if (itemstack.getItem() == Items.CROSSBOW) {
+			{
+				Entity _ent = entity;
+				if (!_ent.level().isClientSide() && _ent.getServer() != null) {
+					_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level() instanceof ServerLevel ? (ServerLevel) _ent.level() : null, 4,
+							_ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent), "kick @p Crossbows are banned on this server");
+				}
+			}
+			if (entity instanceof Player _player) {
+				ItemStack _stktoremove = new ItemStack(Items.CROSSBOW);
+				_player.getInventory().clearOrCountMatchingItems(p -> _stktoremove.getItem() == p.getItem(), 1, _player.inventoryMenu.getCraftSlots());
 			}
 		}
 	}
